5b90e27a7a73cdcf3da6b56939906fa3735d82db
Maitri B <maitri_b@hotmail.com>
2026-07-10 03:29:08 +0700

    Update GAS upload slip URL to new deployment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@
 
 // Google Apps Script Web App URL for uploading payment slips
 export const GAS_UPLOAD_SLIP_URL = process.env.NEXT_PUBLIC_GAS_UPLOAD_SLIP_URL ||
-  'https://script.google.com/macros/s/AKfycbx5ML4iBZF_6zeFQ94EEhlP2oZr8w8Fq6hSr_3rjHyrPVeQN2Dxvtb1gUAOU3JFstDZBQ/exec';
+  'https://script.google.com/macros/s/AKfycby01CsVrFIfQzzJ8wWTgakXbxzdv1ZXDrgzFQyHiM1B2qCJGO6rD0_Bqtbx-HOu_VwiAw/exec';
 
 // Admin LINE contact
 export const ADMIN_LINE_URL = 'https://lin.ee/nzAjXXq';
